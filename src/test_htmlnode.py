3e500e2160c44406e4e68abce92f67f175a3715a
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2026-02-14 18:42:14 -0700

    Added leaf node

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,5 +1,5 @@
 import unittest
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
@@ -19,3 +19,22 @@ class TestHTMLNode(unittest.TestCase):
         }
         )
         print(html_node)
+
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_with_multiple_props(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com" target="_blank">Click me!</a>'
+        )
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(
+            node.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
