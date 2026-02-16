bc7619d2a61f957c038e8eb48ce5a3bb0b806f63
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2026-02-16 08:55:09 -0800

    Update html node

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -37,3 +37,18 @@ class LeafNode(HTMLNode):
         return f"Tag: {self.tag},\nValue: {self.value}, \nProps: {self.props}"
 
 
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag=tag, value=None, children=children, props=props)
+        
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError
+        if self.children is None:
+            raise ValueError("")
+        else:
+            combined_str = ""
+            for child in self.children:
+                combined_str += child.to_html()
+            return f"<{self.tag}>{combined_str}</{self.tag}>"
