479fc8ca13684175c6ea2afda429866b98e7f98a
Ahmad-Omar-Ahsan <ahmadomarahsan@gmail.com>
2026-02-17 08:31:08 -0800

    Update src coude

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,5 +1,5 @@
 import unittest
-from textnode import TextNode, TextType
+from textnode import TextNode, TextType, text_node_to_html_node
 
 class TestTextNode(unittest.TestCase):
     def test_eq(self):
@@ -11,6 +11,12 @@ class TestTextNode(unittest.TestCase):
         node = TextNode("This is link", TextType.LINK)
         node2 = TextNode("This is another link", TextType.LINK)
         self.assertEqual(node.text_type, node2.text_type)
+    
+    def test_text(self):
+        node = TextNode("This is a text node", TextType.TEXT)
+        html_node = text_node_to_html_node(node)
+        self.assertEqual(html_node.tag, None)
+        self.assertEqual(html_node.value, "This is a text node")
 
 
 
